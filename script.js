798da4d56960bd48858045ee4b27e46ed6eedb48
Oscar A.G <>
2021-05-10 19:35:56 +0000

    Oops the linter didn't like something with the boloen so I fixed it

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function Activate () {
 
   urage = parseInt(urage)
 
-  if ((urday ==='Tuesday' || urday === 'Thursday') || (urage >= 12 && urage <= 21)) {
+  if ((urday === 'Tuesday' || urday === 'Thursday') || (urage >= 12 && urage <= 21)) {
     document.getElementById('output').innerHTML = 'You get student pricing! At 4.99$ an addmission!'
   } else {
     document.getElementById('output').innerHTML = 'Sadly you get regular pricing at 7.99$ an addmission'
