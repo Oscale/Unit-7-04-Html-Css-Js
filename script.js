71b8366cb1e585a17ec06c683b0ef1e95e3ce885
Oscar A.G <>
2021-05-10 19:32:03 +0000

    Cleaning up the program to look nice and please the linter

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function Activate () {
 
   urage = parseInt(urage)
 
-  if ((urday == 'Tuesday' || urday == 'Thursday') || (urage >= 12 && urage <= 21)) { 
-    document.getElementById('output').innerHTML = 'You get student pricing! At 4.99\$ an addmission!'
+  if ((urday ==='Tuesday' || urday === 'Thursday') || (urage >= 12 && urage <= 21)) {
+    document.getElementById('output').innerHTML = 'You get student pricing! At 4.99$ an addmission!'
   } else {
-    document.getElementById('output').innerHTML = 'Sadly you get regular pricing at 7.99\$ an addmission'
+    document.getElementById('output').innerHTML = 'Sadly you get regular pricing at 7.99$ an addmission'
   }
 }
